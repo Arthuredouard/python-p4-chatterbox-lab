f9864dee96454ad4105f438dbd2b379fdb8d4d09
unknown <marcarthur.dev@gmail.com>
2025-10-10 15:29:08 -0400

    update

--- server/app.py
+++ server/app.py
@@ -1,29 +1,3 @@
-# from flask import Flask, request, make_response, jsonify
-# from flask_cors import CORS
-# from flask_migrate import Migrate
-
-# from models import db, Message
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.json.compact = False
-
-# CORS(app)
-# migrate = Migrate(app, db)
-
-# db.init_app(app)
-
-# @app.route('/messages')
-# def messages():
-#     return ''
-
-# @app.route('/messages/<int:id>')
-# def messages_by_id(id):
-#     return ''
-
-# if __name__ == '__main__':
-#     app.run(port=5555)
 # server/app.py
 
 from flask import Flask, request, jsonify
